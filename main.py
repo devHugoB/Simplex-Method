52055ebe2dda7fd2fcac01f853692a2709f332a3
Sikariuss <sikariussumbra@gmail.com>
2021-03-24 10:02:14 +0100

    Simplex de première espèce
    Permet de calculer le maximum de la fonction économique grâce au simplex de première espèce.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # utf-8
 
 ###################
+# IMPORTS
+###################
+
+from fractions import Fraction
+
+
+###################
 # SIMPLEX
 ###################
 
@@ -14,70 +21,216 @@ class Simplex:
         self.above = above
         self.inside = inside
         self.lines = lines
+        self.tab_ln = []
 
     def gen_ln(self):
-        self.first_ln = self.func + self.e_top
-        self.second_ln = self.constraints[0] + self.e_inside[0]
-        self.third_ln = self.constraints[1] + self.e_inside[1]
-        self.fourth_ln = self.constraints[2] + self.e_inside[2]
+        self.tab_ln.append(self.func + self.e_top)
+        for i in range(len(self.constraints)):
+            self.tab_ln.append(self.constraints[i] + self.e_inside[i])
 
     def gen_tab(self):
+        if str(self.values[0]).startswith('-') or self.values[0] == 0:
+            value_z = self.values[0] * -1
+            sign = '-'
+        else:
+            value_z = self.values[0]
+            sign = '+'
 
+        for ln in range(len(self.tab_ln)):
+            for cl in range(len(self.tab_ln[ln])):
+                float_current_number = float(self.tab_ln[ln][cl])
+                if str(float_current_number).endswith('.0'):
+                    self.tab_ln[ln][cl] = int(self.tab_ln[ln][cl])
+
+        space = '     '
+        line_tab = '--------' * (len(self.above) + 1)
         tab = [
-            ['  ', self.above[0], self.above[1], self.above[2], self.above[3], self.above[4]],
-            ['  ', self.first_ln, f'Z-{self.values[0]}', self.lines[0]],
-            [self.inside[0], self.second_ln, self.values[1], self.lines[1]],
-            [self.inside[1], self.third_ln, self.values[2], self.lines[2]],
-            [self.inside[2], self.fourth_ln, self.values[3], self.lines[3]]
+            ['     '],
+            ['     ', '', f'Z{sign}{value_z}', ' | ', self.lines[0]],
+            ['      ' + line_tab]
         ]
+        for i in range(len(self.tab_ln[0])):
+            tab[1][1] += str(self.tab_ln[0][i]) + space + '| '
 
-        for i in range(len(tab)):
-            for j in range(len(tab[i])):
-                print(tab[i][j], end=' ')
+        tab[0] += [self.above[i] + '    |' for i in range(len(self.above))]
+
+        for i in range(len(self.inside)):
+            tab.append([self.inside[i], '  ', '', self.values[i + 1], '| ', self.lines[i + 1]])
+            for val in range(len(self.tab_ln[i+1])):
+                tab[i+3][2] += str(self.tab_ln[i+1][val]) + space + '| '
+
+        for ln in range(len(tab)):
+            for cl in range(len(tab[ln])):
+                print(tab[ln][cl], end=' ')
             print()
 
     def test_next_tab(self):
         self.gen_tab()
 
-        max_value = max(self.first_ln)
-        idx = self.first_ln.index(max_value)
+        for i in range(len(self.lines)):
+            self.lines[i] = f'l{i}'
+
+        max_value = max(self.tab_ln[0])
+        idx = self.tab_ln[0].index(max_value)
 
         if max_value > 0:
             self.calc_next_tab(max_value, idx)
         else:
-            print('\nFin de la simulation')
+            print('\n----- FIN DES CALCULS -----\n')
             tab_final = [['z0', self.values[0]]]
 
             for i in range(len(self.above)):
-                if i > 2:
-                    value_final = 0
+                if self.above[i] in self.inside:
+                    value_final = self.values[self.inside.index(self.above[i]) + 1]
                 else:
-                    value_final = self.values[i + 1]
+                    value_final = 0
 
                 tab_final.append([self.above[i], value_final])
+            tab_final[0][1] *= -1
 
             for i in range(len(tab_final)):
-                print(tab_final[i][0], end=' |')
-            print()
-            for j in range(len(tab_final)):
-                print(tab_final[j][1], end='  |')
+                tab_final[i][1] = str(tab_final[i][1])
+                print(f'{tab_final[i][0]} : {tab_final[i][1]}')
+
+            input('\n\nAppuyer sur une touche pour quitter...')
 
     def calc_next_tab(self, val_top, idx):
-        cl = [val_top, self.second_ln[idx], self.third_ln[idx], self.fourth_ln[idx]]
+        cl = [val_top]
+        for i in range(1, len(self.tab_ln)):
+            cl.append(self.tab_ln[i][idx])
+
         tab_value = []
         for i in range(1, len(cl)):
-            tab_value.append(cl[i] / self.values[i])
+            if cl[i] > 0:
+                tab_value.append(self.values[i] / cl[i])
+                tab_value_after_point = str(float(tab_value[i-1])).split('.')[1]
+                if any(tab_value_after_point.count(x) > 1 for x in tab_value_after_point):
+                    tab_value[i-1] = Fraction(self.values[i], cl[i])
+                else:
+                    tab_value[i-1] = round(float(tab_value[i-1]), 2)
+            else:
+                tab_value.append(100000000)
 
         min_value = min(tab_value)
         idx_min = tab_value.index(min_value)
+        current_ln = self.tab_ln[idx_min + 1]
+        current_number = cl[idx_min + 1]
+        current_number_idx = current_ln.index(current_number)
 
-        if idx_min == 0:
-            current_ln = self.second_ln
-        elif idx_min == 1:
-            current_ln = self.third_ln
-        elif idx_min == 2:
-            current_ln = self.fourth_ln
+        self.inside[idx_min] = above[current_number_idx]
 
+        self.calc_new_lines(current_ln, current_number_idx)
+
+    def calc_new_lines(self, current_ln, idx):
+        new_current_ln = []
+        new_value = -1
+        current_name_ln = self.lines[self.tab_ln.index(current_ln)]
+        current_val_ln = current_ln
+
+        for i in range(len(self.tab_ln)):
+            if (self.tab_ln[i][idx] != 0 and current_val_ln != self.tab_ln[i]) or \
+                    (current_val_ln == self.tab_ln[i] and current_val_ln[idx] != 1):
+
+                if self.tab_ln[i][idx] != 0 and current_val_ln != self.tab_ln[i]:
+                    factor = float(self.tab_ln[i][idx]) / current_val_ln[idx]
+
+                    if any(str(float(factor)).split('.')[1].count(x) > 1 for x in str(float(factor)).split('.')[1]):
+                        if type(self.tab_ln[i][idx]) != Fraction and type(current_val_ln[idx]) != Fraction:
+                            if type(self.tab_ln[i][idx]) == float:
+                                self.tab_ln[i][idx] = Fraction(self.tab_ln[i][idx])
+                            if type(current_val_ln[idx]) == float:
+                                current_val_ln[idx] = Fraction(current_val_ln[idx])
+
+                            factor = Fraction(self.tab_ln[i][idx], current_val_ln[idx])
+                    else:
+                        factor = float(factor)
+
+                else:
+                    factor = 1 / current_val_ln[idx]
+
+                    if any(str(float(factor)).split('.')[1].count(x) > 1 for x in str(float(factor)).split('.')[1]):
+                        if type(current_val_ln[idx]) != Fraction:
+                            if type(current_val_ln[idx]) == float:
+                                current_val_ln[idx] = Fraction(current_val_ln[idx])
+
+                            factor = Fraction(1, current_val_ln[idx])
+                    else:
+                        factor = float(factor)
+
+                if str(factor).endswith('.0'):
+                    factor = int(factor)
+
+                elif '.' in str(factor) and len(str(factor).split('.')[1]) > 2:
+                    factor = round(factor, 2)
+
+                current_ln_with_factor = [i * abs(factor) for i in current_val_ln]
+
+                if self.tab_ln[i][idx] > 0 and current_val_ln != self.tab_ln[i]:
+                    for val in range(len(self.tab_ln[i])):
+                        self.tab_ln[i][val] -= current_ln_with_factor[val]
+
+                elif self.tab_ln[i][idx] < 0 and current_val_ln != self.tab_ln[i]:
+                    for val in range(len(self.tab_ln[i])):
+                        self.tab_ln[i][val] += current_ln_with_factor[val]
+
+                else:
+                    for val in range(len(self.tab_ln[i])):
+                        new_current_ln.append(current_ln_with_factor[val])
+
+                for val in range(len(self.tab_ln[i])):
+                    if str(self.tab_ln[i][val]).endswith('.0'):
+                        self.tab_ln[i][val] = int(self.tab_ln[i][val])
+
+                    elif '.' in str(self.tab_ln[i][val]) and len(str(self.tab_ln[i][val]).split('.')[1]) > 2:
+                        self.tab_ln[i][val] = round(self.tab_ln[i][val], 2)
+
+                if str(factor).startswith('-'):
+                    sign = '+'
+                else:
+                    sign = '-'
+
+                if factor != 1 and factor != -1 and self.tab_ln[i] != current_val_ln:
+                    self.lines[i] += f' {sign}{abs(factor)} {current_name_ln}'
+                elif self.tab_ln[i] != current_val_ln:
+                    self.lines[i] += f' {sign}{current_name_ln}'
+                else:
+                    self.lines[i] = f'{abs(factor)} {current_name_ln}'
+
+                if self.tab_ln[i] != current_val_ln:
+                    self.values[i] -= (factor * self.values[self.tab_ln.index(current_val_ln)])
+                else:
+                    new_value = factor * self.values[self.tab_ln.index(current_val_ln)]
+
+                if str(self.values[i]).endswith('.0'):
+                    self.values[i] = int(self.values[i])
+
+                elif '.' in str(self.values[i]) and len(str(self.values[i]).split('.')[1]) > 2:
+                    self.values[i] = round(self.values[i], 2)
+
+            else:
+                continue
+
+        if new_value != -1:
+            if str(new_value).endswith('.0'):
+                new_value = int(new_value)
+
+            elif '.' in str(new_value) and len(str(new_value).split('.')[1]) > 2:
+                new_value = round(new_value, 2)
+
+            self.values[self.tab_ln.index(current_ln)] = new_value
+
+        if len(new_current_ln) > 0:
+            for i in range(len(new_current_ln)):
+                if str(new_current_ln[i]).endswith('.0'):
+                    new_current_ln[i] = int(new_current_ln[i])
+
+                elif '.' in str(new_current_ln[i]) and len(str(new_current_ln[i]).split('.')[1]) > 2:
+                    new_current_ln[i] = round(new_current_ln[i], 2)
+
+            self.tab_ln[self.tab_ln.index(current_ln)] = new_current_ln
+
+        print('\n~~~~~~~~~~~~~~~ TABLEAU SUIVANT ~~~~~~~~~~~~~~~\n')
+        self.test_next_tab()
 
 
 ###################
@@ -85,15 +238,53 @@ class Simplex:
 ###################
 
 if __name__ == "__main__":
-    function = [-1, 0]
-    contraintes = [[1, -1], [1, 0], [0, 1]]
-    value = [0, 1, 2, 2]
-    e_top = [0, 0, 0]
-    e_inside = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-    first_line = function + e_top
-    above = ['x1', 'x2', 'e1', 'e2', 'e3']
-    inside = ['e1', 'e2', 'e3']
-    line = ['l0', 'l1', 'l2', 'l3']
+    function = []
+    contraintes = []
+    e_top = []
+    e_inside = []
+    inside = []
+    value = [0]
+
+    nb_function = int(input('Combien il y a de x dans la fonction ? '))
+    above = [f'x{i + 1}' for i in range(nb_function)]
+
+    print()
+    for i in range(nb_function):
+        function.append(float(input(f'Combien vaut x{i + 1} ? ')))
+        if str(function[i]).endswith('.0'):
+            function[i] = int(function[i])
+
+    nb_contraintes = int(input('\nCombien il y a de contraintes (hors x1, x2, ..., xn >= 0) ? '))
+    line = [f'l{i}' for i in range(nb_contraintes + 1)]
+
+    for i in range(nb_contraintes):
+        print()
+        new_contraintes = []
+        for j in range(nb_function):
+            new_contraintes.append(float(input(f'Combien vaut x{j + 1} dans la contrainte {i + 1} ? ')))
+            if str(new_contraintes[j]).endswith('.0'):
+                new_contraintes[j] = int(new_contraintes[j])
+
+        contraintes.append(new_contraintes)
+
+        value.append(float(input(f'Combien vaut la contrainte {i + 1} ? ')))
+        if str(value[i + 1]).endswith('.0'):
+            value[i + 1] = int(value[i + 1])
+
+    for i in range(len(contraintes)):
+        above.append(f'e{i + 1}')
+        inside.append(f'e{i + 1}')
+        e_top.append(0)
+
+        new_e_inside = []
+        for j in range(len(contraintes)):
+            if j != i:
+                new_e_inside.append(0)
+            else:
+                new_e_inside.append(1)
+        e_inside.append(new_e_inside)
+
+    print('\n----- DEBUT DES CALCULS -----\n')
 
     simplex = Simplex(function, contraintes, value, e_top, e_inside, above, inside, line)
     simplex.gen_ln()
